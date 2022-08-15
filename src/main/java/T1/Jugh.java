ac84756aeddc19e224cb51beb43908095b0bcf98
tan <45691158@qq.com>
2022-08-15 15:01:11 +0800

    github1

--- src/main/java/T1/Jugh.java
+++ src/main/java/T1/Jugh.java
@@ -8,5 +8,6 @@ public class Jugh {
         System.out.println("第三次");
         System.out.println("hot分支");
         System.out.println("hot冲突");
+        System.out.println("github推送版");
     }
 }
